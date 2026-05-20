f26ed7b2e57efad114c7b6a245967cb6f04d55e6
Bary <mbary92@gmail.com>
2026-05-20 20:23:27 +0200

    init

---
+++ src/main/java/com/mbarydev/slowle/SlowleApplication.java
@@ -0,0 +1,13 @@
+package com.mbarydev.slowle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlowleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlowleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mbarydev/slowle/SlowleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbarydev.slowle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlowleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
